7d4ff87360a8158a954273c7f066f3bc5193a600
Evan Swanson <evan.sundelius@gmail.com>
2022-08-26 11:01:28 -0500

    complete book.js section

--- src/book.js
+++ src/book.js
@@ -14,18 +14,34 @@ function buildMainCharacter(name, age, pronouns) {
 function saveReview(statement, listOfReviews) {
   if (listOfReviews.includes(statement) === false) {
   return listOfReviews.push(statement);
-}
+};
 };
 
-function calculatePageCount() {
+function calculatePageCount(bookTitle) {
+  var pageCount = bookTitle.length * 20
+  return pageCount
+};
 
-}
+function writeBook(bookTitle, builtCharacter, bookGenre) {
+  var bookObject = {
+    title: bookTitle,
+    mainCharacter: builtCharacter,
+    pageCount: bookTitle.length * 20,
+    genre: bookGenre
+  };
+  return bookObject
+};
+
+function editBook(bookObject) {
+  return bookObject.pageCount = bookObject.pageCount * .75
+};
+
 
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
   calculatePageCount,
-  // writeBook,
-  // editBook
+  writeBook,
+  editBook
 }
