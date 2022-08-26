89bfb4d7725c68a596e1f273557cfae8c7249d21
Evan Swanson <evan.sundelius@gmail.com>
2022-08-26 10:30:32 -0500

    add progress up through saveReview

--- src/book.js
+++ src/book.js
@@ -2,15 +2,30 @@ function createTitle(title) {
   return `The ${title}`;
 };
 
-function buildMainCharacter() {
-  
+function buildMainCharacter(name, age, pronouns) {
+  var builtCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  };
+  return builtCharacter;
+};
+
+function saveReview(statement, listOfReviews) {
+  if (listOfReviews.includes(statement) === false) {
+  return listOfReviews.push(statement);
+}
+};
+
+function calculatePageCount() {
+
 }
 
 module.exports = {
   createTitle,
   buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
+  saveReview,
+  calculatePageCount,
   // writeBook,
   // editBook
 }
