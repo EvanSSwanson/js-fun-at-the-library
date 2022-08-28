05418f1913eed0de04f2d69a565843d91bc1610e
Evan Swanson <evan.sundelius@gmail.com>
2022-08-28 14:55:15 -0500

    Complete shelf section

--- src/shelf.js
+++ src/shelf.js
@@ -5,14 +5,41 @@ function shelfBook(book, sciFiShelf) {
   };
 };
 
-function unshelfBook() {
+function unshelfBook(bookTitle, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title.includes(bookTitle))
+      shelf.splice(i,1)
+    };
+};
 
+function listTitles(shelf) {
+  var titlesList = []
+  for (var i = 0; i < shelf.length; i++) {
+    var list = shelf[i].title
+
+    titlesList.push(list)
+  }
+
+return titlesList.join(', ')
+};
+
+
+
+function searchShelf(shelf, bookTitle) {
+    for (var i = 0; i < shelf.length; i++)
+      var titleKey = shelf[i].title
+    {
+      if (titleKey.includes(bookTitle)) {
+        return true;
+      } else {
+        return false;
+      }
+  }
 }
-
 
 module.exports = {
   shelfBook,
   unshelfBook,
-  // listTitles,
-  // searchShelf
+  listTitles,
+  searchShelf
 };
